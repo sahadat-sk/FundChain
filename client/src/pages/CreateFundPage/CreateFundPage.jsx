224a20862a53ad1e05844ee0a57990b533f711cd
AritriBiswas <aritribiswas07@gmail.com>
2022-10-30 14:56:09 +0530

    Create fund page

--- client/src/pages/CreateFundPage/CreateFundPage.jsx
+++ client/src/pages/CreateFundPage/CreateFundPage.jsx
@@ -53,51 +53,53 @@ const CreateFundPage = () => {
     return (
         <>
             <h1 className="create-fund-heading">Create Your Own Fund</h1>
-            <form>
-                <label>
+            <div className="full-form">
+            <form className="form-fields">
+                <label className="charity-label">
                     Charity Name:
                     <input
-                        type="text"
+                        type="text" className="box"
                         value={charityName}
                         onChange={(e) => setCharityName(e.target.value)}
                     />
                 </label>
-                <label>
+                <label className="charity-label">
                     Owner Address:
                     <input
-                        type="text"
+                        type="text" className="box"
                         value={ownerAddress}
                         onChange={(e) => setOwnerAddress(e.target.value)}
                     />
                 </label>
-                <label>
+                <label className="charity-label">
                     Amount Required:
                     <input
-                        type="text"
+                        type="text" className="box"
                         value={amountRequired}
                         onChange={(e) => setAmountRequired(e.target.value)}
                     />
                 </label>
-                <label>
+                <label className="charity-label">
                     Min amount to donate:
                     <input
-                        type="text"
+                        type="text" className="box"
                         value={minAmountToDonate}
                         onChange={(e) => setMinAmountToDonate(e.target.value)}
                     />
                 </label>
-                <label>
+                <label className="charity-label">
                     Description:
                     <input
-                        type="text"
+                        type="text" className="box" id="desc-box" style={{height:"27rem", width:"91rem"}}
                         value={discription}
                         onChange={(e) => setDiscription(e.target.value)}
                     />
                 </label>
             </form>
-           {!isLoading? <button className="create-fund-btn" onClick={handleSubmit}>
+           {isLoading? <button className="create-fund-btn" onClick={handleSubmit}>
                 Create
-            </button>:<p>Loading...</p>}
+            </button>:<p className="loading">Loading...</p>}
+        </div>
         </>
     );
 };
